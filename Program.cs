8da8760f3d130935a12b769978d515aa022a36e1
ThangNguyen312v <tatthang112113@gmail.com>
2024-08-12 21:33:48 +0700

    baitap

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace baitapfor{
+    class Program{
+        static void Main(string[] Args){
+            int dong = 3;
+            int cot = 7;
+
+            for(int i=0;i<=dong; i++){
+                Console.WriteLine("");
+                for(int j=0; j<=cot; j++){
+                    Console.Write("*");
+                }
+
+            }
+
+        }
+    }
+}
